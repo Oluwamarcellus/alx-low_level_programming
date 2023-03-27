46b4d4c14f70c4af0578d42af11c47c27b609ac9
Ayowale <haydexwisdom@gmail.com>
2023-03-27 21:52:33 +0000

    C pointers and array task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*puts_half - main func
+*@str: arg
+*Return: void
+*/
+
+
+void puts_half(char *str)
+{
+	int i, j, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	j = (len / 2);
+	if ((len % 2) == 1)
+		j = ((len + 1) / 2);
+	for (i = j; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
