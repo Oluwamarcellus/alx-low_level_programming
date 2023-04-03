cd66cb7fdc1351c8fb478a762d3741f7293f8280
Ayowale <haydexwisdom@gmail.com>
2023-04-03 15:16:36 +0000

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*_strstr - locates a substring
+*@haystack: string
+*@needle: string
+*Return: char
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
