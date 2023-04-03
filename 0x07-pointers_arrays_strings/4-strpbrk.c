590447fe26a3cdaf439646941c1b88ba06177942
Ayowale <haydexwisdom@gmail.com>
2023-04-03 14:42:33 +0000

    task 4 fixed

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,10 +10,10 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	while (s[i++] >= '\0')
+	while (s[i++])
 	{
 		j = 0;
-		while (accept[j++] > '\0')
+		while (accept[j++])
 		{
 			if (s[i] == accept[j])
 				return (s + i);
