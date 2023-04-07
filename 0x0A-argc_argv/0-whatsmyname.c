52a5fb5f42d7e6d59c65dde99ce62386c3f66135
Ayowale <haydexwisdom@gmail.com>
2023-04-07 14:28:31 +0000

    C argc and argv task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry point
+*@argc: command line arguements counts
+*@argv: array of command line arguements
+*Return: 0
+*/
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
