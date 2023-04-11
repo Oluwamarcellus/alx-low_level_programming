dba0bbc93d40d6f51414053487a1d0a9c6167255
Ayowale <haydexwisdom@gmail.com>
2023-04-11 15:51:00 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,35 +7,33 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int s1_len = 0, s2_len = 0, g_len, i = 0;
+	int s1_len = 0, s2_len = 0, g_len, i = 0, j = 0;
 	char *a;
 
+	if (s1 == NULL)
+		s1 = "";
 	while (s1[s1_len] != '\0')
-	{
 		s1_len++;
-	}
+	if (s2 == NULL)
+		s2 = "";
 	while (s2[s2_len] != '\0')
-	{
 		s2_len++;
-	}
 	g_len = (s1_len + s2_len);
 	a = malloc(sizeof(char) * (g_len + 1));
 	if (a == 0)
 		return (0);
-	s1_len = 0;
-	s2_len = 0;
-	while (s1[s1_len] != '\0')
+	while (j < s1_len)
 	{
-		a[i] = s1[s1_len];
+		a[i] = s1[j];
 		i++;
-		s1_len++;
+		j++;
 	}
-	while (s2[s2_len] != '\0')
+	j = 0;
+	while (j < s2_len)
 	{
-		a[i] = s2[s2_len];
+		a[i] = s2[j];
 		i++;
-		s2_len++;
+		j++;
 	}
 	return (a);
-
 }
