e68d772ea305c61d13e9072d81b0f60abcad9d53
Ayowale <haydexwisdom@gmail.com>
2023-04-11 12:26:20 +0000

    task 0 fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,17 +14,15 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	{
 		return (0);
-	} else
+	}
+	a = malloc(sizeof(char) * size);
+	if (a == NULL)
 	{
-		a = malloc(sizeof(char) * size);
-		if (a == NULL)
-		{
-			return (0);
-		}
-		for (i = 0; i < size; i++)
-		{
-			a[i] = c;
-		}
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
 	}
 	return (a);
 }
