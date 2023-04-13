951c383bf15762bb74b98d68a3444d27191a1011
Ayowale <haydexwisdom@gmail.com>
2023-04-13 13:45:24 +0000

    ALX C more malloc task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+*malloc_checked - allocates memory using malloc
+*@b: size
+*Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
