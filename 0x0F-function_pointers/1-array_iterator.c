fa54d0de15579c39bf5e0085e8d85c152bc5f8c5
Ayowale <haydexwisdom@gmail.com>
2023-04-19 12:54:16 +0000

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+/**
+*array_iterator - executes a passed in functn on each elm of an array
+*@array: array
+*@size: size of the array
+*@action: the function
+*Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
