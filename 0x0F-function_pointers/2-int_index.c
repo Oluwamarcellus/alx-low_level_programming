af424b5c4ee0a5c89ed25691918a156b26dc9781
Ayowale <haydexwisdom@gmail.com>
2023-04-19 16:34:45 +0000

    task 2 fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == 0 || cmp == 0)
-		return (0);
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
