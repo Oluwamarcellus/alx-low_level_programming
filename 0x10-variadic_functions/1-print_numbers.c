72e686fa2d9b0e0f8f9529ff5b87ce93609fbd4f
Ayowale <haydexwisdom@gmail.com>
2023-04-20 22:06:11 +0000

    task 1 fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 	
-	if (separator == 0)
-		return;
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
