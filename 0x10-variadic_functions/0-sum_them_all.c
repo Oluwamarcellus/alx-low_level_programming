b733ada47d81dec194ce9c0cc04c163fe9f221cb
Ayowale <haydexwisdom@gmail.com>
2023-04-20 21:49:26 +0000

    ALX C Variadic function task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+/**
+*sum_them_all - returns the sum of all its parameters
+*@n: size of args
+*Return: int
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
