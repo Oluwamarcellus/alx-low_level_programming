e32751f7349b61b4beece7caa4b16ddc03b64a70
Ayowale <haydexwisdom@gmail.com>
2023-04-20 23:52:53 +0000

    task 3 fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,23 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
-*_separator - separates output
-*@format: format
-*@i: iterator
-*Return: void
-*/
-void _separator(const char * const format, int i)
-{
-	if (format[i + 1] != 0 &&
-	(format[i] == 'c' ||
-	format[i] == 'i' ||
-	format[i] == 'f' ||
-	format[i] == 's'))
-	{
-		printf(", ");
-	}
-}
-/**
 *print_all - unction that prints anything
 *@format: list of types of arguments passed
 *Return: void
@@ -26,8 +9,7 @@ void _separator(const char * const format, int i)
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *str;
-
+	char *str, *sp = "";
 	va_list list;
 
 	va_start(list, format);
@@ -36,26 +18,27 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(list, int));
+				printf("%s%c", sp, va_arg(list, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(list, int));
+				printf("%s%d", sp, va_arg(list, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(list, double));
+				printf("%s%f", sp, va_arg(list, double));
 				break;
 			case 's':
 				str = va_arg(list, char *);
 				if (str == 0)
 					printf("(nil)");
 				else
-					printf("%s", str);
+					printf("%s%s", sp, str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
-		_separator(format, i);
 		i++;
+		sp = ", ";
 	}
 	printf("\n");
 	va_end(list);
