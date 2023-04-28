c39e490090e3ca9c45c20bc0ba82ce3c783f3ac7
Ayowale <haydexwisdom@gmail.com>
2023-04-28 12:08:41 +0000

    task 4

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 /**
 * struct listint_s - singly linked list
 * @n: integer
@@ -19,6 +20,7 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 
 
