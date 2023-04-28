78a621c7e68d98ba27422e8edcce3584a573848d
Ayowale <haydexwisdom@gmail.com>
2023-04-28 20:55:00 +0000

    added task 6 refix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,14 +7,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp = NULL;
-	int i;
+	int n;
 
 	if (*head == NULL)
 		return (0);
 	tmp = *head;
 	n = (*head)->n;
 	*head = (*head)->next;
-	free(tmp;
-	tmp = NULL
+	free(tmp);
+	tmp = NULL;
 	return (n);
 }
