a0ba2083adbe730eee83788961a062e5fb12d6c1
Ayowale <haydexwisdom@gmail.com>
2023-04-30 12:25:06 +0000

    task 9 fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = NULL, *tmp = NULL;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	tmp = *head;
 	while (i < (idx - 1) && tmp != NULL)
