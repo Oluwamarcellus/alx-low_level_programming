ba96715869ca1988eecbcd7d36bce760d98a8eab
Ayowale <haydexwisdom@gmail.com>
2023-05-04 22:02:04 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*get_bit - returns the value of a bit at a given index
+*@index: index to return value of bit
+*Return: value of bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int max_indx_possible = (sizeof(unsigned long int) * 8);
+
+	if (index >= max_indx_possible)
+		return (-1);
+	return ((n >> index) & 1);
+}
