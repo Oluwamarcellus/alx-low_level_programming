57523a13a81e2527b80c703e980dc02a83fb8889
Ayowale <haydexwisdom@gmail.com>
2023-07-09 11:20:58 +0000

    Task 3 worked on

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,22 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *str;
-	unsigned long int idx, i;
-	hash_node_t *new = NULL;
+	unsigned long int idx;
+	hash_node_t *new = NULL, *tmp = NULL;
 
 	if (!key || *key == '\0' || !value || !ht)
 		return (0);
 	str = strdup(value);
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (tmp = ht->array[idx]; tmp; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = str;
+			return (1);
+		}
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 	{
@@ -25,17 +35,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new->key = strdup(key);
 	new->value = str;
-	new->next = NULL;
-	idx = key_index((const unsigned char *)key, ht->size);
-	for (i = idx; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = str;
-			return (1);
-		}
-	}
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
